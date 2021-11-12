db3aef1f8400ce0c9fa1df005ac0db56ffcbce85
Andre Boaventura <andre.monteiro.boaventura95@gmail.com>
2021-11-12 17:10:11 +0000

    SellerFormViewModel and Department select component

--- SalesWebMvc1/Controllers/SellersController.cs
+++ SalesWebMvc1/Controllers/SellersController.cs
@@ -5,16 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc1.Services;
 using SalesWebMvc1.Models;
+using SalesWebMvc1.Models.ViewModels;
 
 namespace SalesWebMvc1.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentServices _departmentServices;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentServices departmentServices)
         {
             _sellerService = sellerService;
+            _departmentServices = departmentServices;
         }
 
         public IActionResult Index()
@@ -25,7 +28,9 @@ namespace SalesWebMvc1.Controllers
 
         public IActionResult Create()
         {
-            return View();  
+            var departments = _departmentServices.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -33,8 +38,9 @@ namespace SalesWebMvc1.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
-            return RedirectToAction(nameof (Index));
+            return RedirectToAction(nameof(Index));
         }
-        
+
+
     }
 }
---
+++ SalesWebMvc1/Services/DepartmentServices.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMvc1.Data;
+using System.Linq;
+using System.Collections.Generic;
+using SalesWebMvc1.Models;
+
+
+namespace SalesWebMvc1.Services
+{
+    public class DepartmentServices
+    {
+        private readonly SalesWebMvc1Context _context;
+
+        public DepartmentServices (SalesWebMvc1Context context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x=> x.Name).ToList();
+        }
+
+
+    }
+}
