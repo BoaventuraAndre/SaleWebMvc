82215ee6a863813a5288c3d71addfba05c13fa50
Andre Boaventura <andre.monteiro.boaventura95@gmail.com>
2021-11-16 16:46:30 +0000

    Grouping Search Implemented

--- SalesWebMvc1/Models/SalesRecord.cs
+++ SalesWebMvc1/Models/SalesRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SalesWebMvc1.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace SalesWebMvc1.Models
@@ -7,7 +8,9 @@ namespace SalesWebMvc1.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
 
