be9c3fe393dd20e5d272f13b20ade62fe30dc5d6
Andre Boaventura <andre.monteiro.boaventura95@gmail.com>
2021-11-16 00:23:09 +0000

    Data formating

--- SalesWebMvc1/Startup.cs
+++ SalesWebMvc1/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -12,6 +13,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc1.Data;
 using SalesWebMvc1.Services;
+using System.Globalization;
+
 
 namespace SalesWebMvc1
 {
@@ -49,6 +52,15 @@ namespace SalesWebMvc1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingservice)
         {
+            var enUs = new CultureInfo("en-US");
+            var localizationOption = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUs),
+                SupportedCultures = new List<CultureInfo> { enUs },
+                SupportedUICultures = new List<CultureInfo> { enUs }
+            };
+
+            app.UseRequestLocalization(localizationOption);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
